88b7a8cf5253b1b882cb342a3af4d2b6d02c81e1
James Jones <jamespaulwjones@gmail.com>
2021-04-21 14:14:22 +0100

    added active admin username and password for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'x20164173@student.ncirl.ie', password: 'F1reblade2021!!', password_confirmation: 'F1reblade2021!!')
